0a99aa752cf0ea110f3886dbda9c5a0b7363dff3
Michelle Stitzer <mcstitzer@ucdavis.edu>
2016-08-01 12:40:53 -0700

    blast and results

--- identify_haplotypes/te_alignments/blast_tes.sh
+++ identify_haplotypes/te_alignments/blast_tes.sh
@@ -3,6 +3,7 @@
 for i in [gdu]*.fa  ### don't want to get the TE db!
 do
 
-blastn -query $i -db TE_12-Feb-2015_15-35.fa -outfmt 6 -max_target_seqs 1 -evalue 1e-20 >> tophit_TEDB_kindrTE.b6.out
+blastn -query $i -db TE_12-Feb-2015_15-35.fa -outfmt 6 -max_target_seqs 1 -evalue 1e-20 -task blastn >> tophit_TEDB_kindrTE.b6.e20.out
+blastn -query $i -db TE_12-Feb-2015_15-35.fa -outfmt 6 -max_target_seqs 1 -evalue 1e-10 -task blastn >> tophit_TEDB_kindrTE.b6.e10.out
 
 done
