cbe09919889664bd4f5a489054de5beb17518d8b
Michelle Stitzer <mcstitzer@ucdavis.edu>
2016-08-30 12:33:04 -0700

    some change

--- kinesin_tree/mrbayes.sh
+++ kinesin_tree/mrbayes.sh
@@ -17,4 +17,8 @@ export PATH=$PATH:/home/mstitzer/software/bin
 # the internet says SLURM will let mpirun know the -n, or --ntasks-per-node
 # https://wiki.bils.se/wiki/Running_MrBayes_at_UPPMAX#Perform_a_parallel_simulation_using_the_MPI-version_of_MrBayes
 ### NOTE that you can only use as many processors as chains you are running, so if 1 run, 4 chains, only 4 CPU.
-mpirun ~/software/mrbayes_3.2.2/src/mb-mpi kinesin_homologs_TranslatorX_Prank_align.100m.mpi.nex > kinesin_homologs_TranslatorX_Prank_align.100m.mpi.log 
+#mpirun ~/software/mrbayes_3.2.2/src/mb-mpi kinesin_homologs_TranslatorX_Prank_align.100m.mpi.nex > kinesin_homologs_TranslatorX_Prank_align.100m.mpi.log 
+
+
+### nucleotide model only
+mpirun ~/software/mrbayes_3.2.2/src/mb-mpi kinesin_homologs_TranslatorX_Prank_align.100m.nuc.nex > kinesin_homologs_TranslatorX_Prank_align.100m.mpi.nex.log 
